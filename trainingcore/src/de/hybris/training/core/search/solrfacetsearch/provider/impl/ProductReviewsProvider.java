24fd056c298cb1eef420467b5a183f27e2663a38
Jorge Sánchez <jorge.sanchez@pivotree.com>
2020-02-16 10:40:15 -0500

    Training-003

---
+++ trainingcore/src/de/hybris/training/core/search/solrfacetsearch/provider/impl/ProductReviewsProvider.java
@@ -0,0 +1,76 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.training.core.search.solrfacetsearch.provider.impl;
+
+import de.hybris.platform.core.model.c2l.CurrencyModel;
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.europe1.jalo.PriceRow;
+import de.hybris.platform.jalo.order.price.PriceInformation;
+import de.hybris.platform.product.PriceService;
+import de.hybris.platform.solrfacetsearch.config.IndexConfig;
+import de.hybris.platform.solrfacetsearch.config.IndexedProperty;
+import de.hybris.platform.solrfacetsearch.config.exceptions.FieldValueProviderException;
+import de.hybris.platform.solrfacetsearch.provider.FieldNameProvider;
+import de.hybris.platform.solrfacetsearch.provider.FieldValue;
+import de.hybris.platform.solrfacetsearch.provider.FieldValueProvider;
+import de.hybris.platform.solrfacetsearch.provider.impl.AbstractPropertyFieldValueProvider;
+import org.springframework.beans.factory.annotation.Required;
+
+import java.util.*;
+
+
+/**
+ * Provides value for volumePrices flag. "true" if product has volume prices, "false" otherwise.
+ */
+public class ProductReviewsProvider extends AbstractPropertyFieldValueProvider implements FieldValueProvider
+{
+	private FieldNameProvider fieldNameProvider;
+
+	@Override
+	public Collection<FieldValue> getFieldValues(final IndexConfig indexConfig, final IndexedProperty indexedProperty,
+			final Object model) throws FieldValueProviderException
+	{
+		Random random = new Random();
+		final ProductModel product = (ProductModel) model;//this provider shall only be used with products
+		final Collection<FieldValue> fieldValues = new ArrayList<FieldValue>();
+
+		Integer numberOfReviews = product.getNumberOfReviews();
+
+		try
+		{
+			addFieldValues(indexedProperty, product, fieldValues, numberOfReviews.intValue() > 0);
+		}catch (Exception ee) {
+			LOG.warn("There was an error creating the indexed property for numberOfReviews");
+		}
+
+		return fieldValues;
+	}
+
+	protected void addFieldValues(final IndexedProperty indexedProperty, final ProductModel product,
+			final Collection<FieldValue> fieldValues, Boolean value) {
+		final Collection<String> fieldNames = getFieldNameProvider().getFieldNames(indexedProperty, null);
+		for (final String fieldName : fieldNames)
+		{
+			fieldValues.add(new FieldValue(fieldName, value));
+		}
+	}
+
+	protected FieldNameProvider getFieldNameProvider()
+	{
+		return fieldNameProvider;
+	}
+
+	@Required
+	public void setFieldNameProvider(final FieldNameProvider fieldNameProvider)
+	{
+		this.fieldNameProvider = fieldNameProvider;
+	}
+}
